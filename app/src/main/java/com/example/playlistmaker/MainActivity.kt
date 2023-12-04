1fa073747daa7d4857a0544751c2a911568b2dfd
Alexander Kireev <sashkir7@mail.ru>
2023-12-04 13:47:39 +0600

    Handle click by menu buttons

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureMenuButtons()
     }
+
+    private fun configureMenuButtons() {
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButtonListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                createToast("Выполняется поиск").show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonListener)
+
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val mediaButtonListener = View.OnClickListener {
+            createToast("Отображается медиатека").show()
+        }
+        mediaButton.setOnClickListener(mediaButtonListener)
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            createToast("Просматриваются настройки").show()
+        }
+    }
+
+    private fun createToast(text: String) = Toast.makeText(
+        this@MainActivity, text, Toast.LENGTH_SHORT
+    )
 }
