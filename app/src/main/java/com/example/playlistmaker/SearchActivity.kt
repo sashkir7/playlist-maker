4809a05d3c3226395812f3d8341e84288043c863
Alexander Kireev <sashkir7@mail.ru>
2024-01-19 12:56:11 +0600

    Save search input data

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.inputmethod.InputMethodManager
@@ -14,6 +13,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
 
+    companion object {
+        private const val SEARCH_VALUE_KEY = "SEARCH_VALUE_KEY"
+    }
+
+    private var searchValue = ""
+
+    private val searchEditText by lazy { findViewById<EditText>(R.id.searchEditText) }
+    private val clearSearchButton by lazy { findViewById<ImageView>(R.id.clearSearchButton) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -21,16 +29,33 @@ class SearchActivity : AppCompatActivity() {
         configureSearchInput()
     }
 
-    private fun configureSearchInput() {
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
-        val clearSearchButton = findViewById<ImageView>(R.id.clearSearchButton)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_VALUE_KEY, searchValue)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchValue = savedInstanceState.getString(SEARCH_VALUE_KEY, "")
+        searchEditText.setText(searchValue)
+    }
+
+    private fun configureSearchInput() {
         clearSearchButton.setOnClickListener {
             searchEditText.text.clear()
             hideKeyboard()
         }
 
-        val textWatcher = searchEditTextTextWatcher(clearSearchButton)
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                clearSearchButton.visibility = if (s.isNullOrEmpty()) GONE else VISIBLE
+                searchValue = s.toString()
+            }
+        }
         searchEditText.addTextChangedListener(textWatcher)
     }
 
@@ -38,17 +63,6 @@ class SearchActivity : AppCompatActivity() {
         currentFocus?.let { view ->
             (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
                 ?.hideSoftInputFromWindow(view.windowToken, 0)
-        }
-    }
-
-    private fun searchEditTextTextWatcher(hideView: View): TextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-        override fun afterTextChanged(s: Editable?) {
-            val visibility = if (s.isNullOrEmpty()) GONE else VISIBLE
-            hideView.visibility = visibility
         }
     }
 }
