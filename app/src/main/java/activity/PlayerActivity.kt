c76042adde8cb2373be715e1cc2b302f60b47252
Alexander Kireev <sashkir7@mail.ru>
2024-04-15 11:39:56 +0600

    Handle track playback

--- app/src/main/java/activity/PlayerActivity.kt
+++ app/src/main/java/activity/PlayerActivity.kt
@@ -1,9 +1,14 @@
 package activity
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import api.dtos.TrackDto
 import api.dtos.coverArtworkUrl
@@ -11,17 +16,37 @@ import api.dtos.releaseDateYear
 import api.dtos.trackTimeAsString
 import com.bumptech.glide.Glide
 import com.example.playlistmaker.R
+import enums.MediaPlayerState.DEFAULT
+import enums.MediaPlayerState.PAUSED
+import enums.MediaPlayerState.PLAYING
+import enums.MediaPlayerState.PREPARED
 import utils.cornerRadius
 import utils.isVisible
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_TRACK_KEY = "EXTRA_TRACK_KEY"
+        private const val TRACK_CURRENT_POSITION_DELAY = 250L
+    }
+
+    private val mainThreadHandler = Handler(Looper.getMainLooper())
+    private val updateDurationValueRunnable = object : Runnable {
+        override fun run() {
+            trackCurrentPosition.text = SimpleDateFormat(
+                "mm:ss", Locale.getDefault()
+            ).format(mediaPlayer.currentPosition)
+            mainThreadHandler.postDelayed(this, TRACK_CURRENT_POSITION_DELAY)
+        }
     }
 
     private val backButton by lazy { findViewById<ImageView>(R.id.iv_back) }
     private val coverImageView by lazy { findViewById<ImageView>(R.id.iv_trackCover) }
+
+    private val playOrPauseButton by lazy { findViewById<ImageView>(R.id.iv_playButton) }
+    private val trackCurrentPosition by lazy { findViewById<TextView>(R.id.tv_trackCurrentPosition) }
 
     private val trackName by lazy { findViewById<TextView>(R.id.tv_trackName) }
     private val artistName by lazy { findViewById<TextView>(R.id.tv_artistName) }
@@ -40,6 +65,9 @@ class PlayerActivity : AppCompatActivity() {
     private val country by lazy { findViewById<TextView>(R.id.tv_countryValue) }
     private val countryGroup by lazy { findViewById<View>(R.id.gr_country) }
 
+    private var playerState = DEFAULT
+    private val mediaPlayer = MediaPlayer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
@@ -50,6 +78,19 @@ class PlayerActivity : AppCompatActivity() {
         configureBackButton()
         configureTrackCover(track.coverArtworkUrl)
         configureTrackInformation(track)
+
+        configurePlayButton()
+        configureMediaPlayer(track.previewUrl)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 
     private fun configureBackButton() = backButton.setOnClickListener { onBackPressed() }
@@ -60,6 +101,16 @@ class PlayerActivity : AppCompatActivity() {
             .cornerRadius(value = 8)
             .placeholder(R.drawable.track_placeholder)
             .into(coverImageView)
+    }
+
+    private fun configurePlayButton() {
+        playOrPauseButton.setOnClickListener {
+            when (playerState) {
+                PLAYING -> pausePlayer()
+                PREPARED, PAUSED -> startPlayer()
+                else -> showPlayerIsNotPreparedToast()
+            }
+        }
     }
 
     private fun configureTrackInformation(track: TrackDto) {
@@ -76,5 +127,38 @@ class PlayerActivity : AppCompatActivity() {
     private fun configureTextView(textView: TextView, groupView: View, value: String?) {
         textView.text = value
         groupView.isVisible = value != null
+    }
+
+    private fun configureMediaPlayer(previewUrl: String) {
+        with(mediaPlayer) {
+            setDataSource(previewUrl)
+            prepareAsync()
+            setOnPreparedListener { playerState = PREPARED }
+            setOnCompletionListener {
+                playerState = PREPARED
+                mainThreadHandler.removeCallbacks(updateDurationValueRunnable)
+                trackCurrentPosition.text = getText(R.string.track_duration_zero)
+                playOrPauseButton.setImageResource(R.drawable.play_icon)
+            }
+        }
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playOrPauseButton.setImageResource(R.drawable.play_icon)
+        playerState = PAUSED
+        mainThreadHandler.removeCallbacks(updateDurationValueRunnable)
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playOrPauseButton.setImageResource(R.drawable.pause_icon)
+        playerState = PLAYING
+        mainThreadHandler.post(updateDurationValueRunnable)
+    }
+
+    private fun showPlayerIsNotPreparedToast() {
+        val text = getText(R.string.media_player_is_not_prepared)
+        Toast.makeText(this, text, LENGTH_SHORT).show()
     }
 }
