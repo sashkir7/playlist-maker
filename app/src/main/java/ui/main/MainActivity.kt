525c87e93c24a71c50fec4252e462d3bf03e149e
Alexander Kireev <sashkir7@mail.ru>
2024-07-29 11:42:06 +0600

    Minor refactoring main activity

--- app/src/main/java/ui/main/MainActivity.kt
+++ app/src/main/java/ui/main/MainActivity.kt
@@ -6,32 +6,33 @@ import ui.settings.SettingsActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import com.example.playlistmaker.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val searchButton by lazy { findViewById<Button>(R.id.search_button) }
+    private val mediaButton by lazy { findViewById<Button>(R.id.media_button) }
+    private val settingsButton by lazy { findViewById<Button>(R.id.settings_button) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        configureMenuButtons()
+        configureSearchButton()
+        configureMediaButton()
+        configureSettingsButton()
     }
 
-    private fun configureMenuButtons() {
-        val searchButton = findViewById<Button>(R.id.search_button)
-        configureStartActivityOnClick(searchButton, SearchActivity::class.java)
-
-        val mediaButton = findViewById<Button>(R.id.media_button)
-        configureStartActivityOnClick(mediaButton, MediaActivity::class.java)
-
-        val settingsButton = findViewById<Button>(R.id.settings_button)
-        configureStartActivityOnClick(settingsButton, SettingsActivity::class.java)
+    private fun configureSearchButton() = searchButton.setOnClickListener {
+        startActivity(Intent(this, SearchActivity::class.java))
     }
 
-    private fun configureStartActivityOnClick(view: View, activityClass: Class<*>) {
-        val intent = Intent(this, activityClass)
-        view.setOnClickListener { startActivity(intent) }
+    private fun configureMediaButton() = mediaButton.setOnClickListener {
+        startActivity(Intent(this, MediaActivity::class.java))
+    }
+
+    private fun configureSettingsButton() = settingsButton.setOnClickListener {
+        startActivity(Intent(this, SettingsActivity::class.java))
     }
 }
