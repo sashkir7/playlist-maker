02c89b1cc55af92cc61895cfb882a7d09153cd4f
Alexander Kireev <sashkir7@mail.ru>
2024-08-19 14:36:50 +0600

    Ui layer media activity

---
+++ app/src/main/java/ui/media/FavoriteTracksFragment.kt
@@ -0,0 +1,21 @@
+package ui.media
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
+
+class FavoriteTracksFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FavoriteTracksFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_favorite_tracks, container, false)
+}
