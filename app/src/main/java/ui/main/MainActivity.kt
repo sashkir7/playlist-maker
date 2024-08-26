c91dd1cf378881e417c9cbf2eb7516fb4d209efe
Alexander Kireev <sashkir7@mail.ru>
2024-08-26 12:03:20 +0600

    Refactoring main activity

--- app/src/main/java/ui/main/MainActivity.kt
+++ app/src/main/java/ui/main/MainActivity.kt
@@ -1,38 +1,26 @@
 package ui.main
 
-import ui.media.MediaActivity
-import ui.search.SearchActivity
-import ui.settings.SettingsActivity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val searchButton by lazy { findViewById<Button>(R.id.search_button) }
-    private val mediaButton by lazy { findViewById<Button>(R.id.media_button) }
-    private val settingsButton by lazy { findViewById<Button>(R.id.settings_button) }
+    private val menu: BottomNavigationView by lazy { findViewById(R.id.bottomNavigationView) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        configureSearchButton()
-        configureMediaButton()
-        configureSettingsButton()
+        configureMenu()
     }
 
-    private fun configureSearchButton() = searchButton.setOnClickListener {
-        startActivity(Intent(this, SearchActivity::class.java))
-    }
-
-    private fun configureMediaButton() = mediaButton.setOnClickListener {
-        startActivity(Intent(this, MediaActivity::class.java))
-    }
-
-    private fun configureSettingsButton() = settingsButton.setOnClickListener {
-        startActivity(Intent(this, SettingsActivity::class.java))
+    private fun configureMenu() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.container_view) as NavHostFragment
+        menu.setupWithNavController(navHostFragment.navController)
     }
 }
