30736b03bc2e8940421797341fb453750258ee8c
Alexander Kireev <sashkir7@mail.ru>
2024-10-22 10:53:43 +0300

    Navigate to new playlist fragment

--- app/src/main/java/ui/media/playlists/PlaylistsFragment.kt
+++ app/src/main/java/ui/media/playlists/PlaylistsFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
@@ -14,11 +16,32 @@ class PlaylistsFragment : Fragment() {
         fun newInstance() = PlaylistsFragment()
     }
 
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: PlaylistsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_playlists, container, false)
+    ): View {
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configureNewPlaylistButton()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun configureNewPlaylistButton() = binding.newPlaylistButton.setOnClickListener {
+        findNavController().navigate(R.id.action_mediaFragment_to_newPlaylistFragment)
+    }
 }
---
+++ app/src/main/java/ui/media/playlists/new/NewPlaylistViewModel.kt
@@ -0,0 +1,5 @@
+package ui.media.playlists.new
+
+import androidx.lifecycle.ViewModel
+
+class NewPlaylistViewModel : ViewModel()
