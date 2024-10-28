cf0e63ad33edf0c81154b4561259654ee9f58bc7
Alexander Kireev <sashkir7@mail.ru>
2024-10-29 00:18:10 +0300

    Fix ending convertor

--- app/src/main/java/utils/EndingConvertor.kt
+++ app/src/main/java/utils/EndingConvertor.kt
@@ -4,9 +4,9 @@ object EndingConvertor {
 
     fun track(amount: Int): String {
         val value = when {
+            amount % 100 in 11..19 -> "треков"
             amount % 10 == 1 -> "трек"
             amount % 10 in 2..4 -> "трека"
-            amount % 100 in 11..19 -> "треков"
             else -> "треков"
         }
         return "$amount $value"
@@ -14,9 +14,9 @@ object EndingConvertor {
 
     fun minute(amount: Int): String {
         val value = when {
+            amount % 100 in 11..19 -> "минут"
             amount % 10 == 1 -> "минута"
             amount % 10 in 2..4 -> "минуты"
-            amount % 100 in 11..19 -> "минут"
             else -> "минут"
         }
         return "$amount $value"
